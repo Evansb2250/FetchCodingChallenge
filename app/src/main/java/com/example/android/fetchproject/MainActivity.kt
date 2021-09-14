0372e982f95979aa27e47a3866d0eb82b280824d
samue <samuebrandenburg12@gmail.com>
2021-09-14 15:23:23 -0500

    * implemented Filter

--- app/src/main/java/com/example/android/fetchproject/MainActivity.kt
+++ app/src/main/java/com/example/android/fetchproject/MainActivity.kt
@@ -4,24 +4,35 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
+import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.RecyclerView
+import com.example.android.fetchproject.databinding.ActivityMainBinding
 import com.example.android.fetchproject.recyclerview.AccountAdapter
 import com.example.android.fetchproject.repository.Repository
 
 class MainActivity : AppCompatActivity() {
     lateinit var adapter:AccountAdapter
+    lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        val vm = ViewModelProvider(this).get(MainActivityViewModel::class.java)
-        val recyclerView = findViewById<RecyclerView>(R.id.recylerView)
+        binding = DataBindingUtil.setContentView(this ,R.layout.activity_main)
 
+        val vm = ViewModelProvider(this).get(MainActivityViewModel::class.java)
+        binding.viewModel = vm
+        binding.setLifecycleOwner(this)
+
+        vm.completeListOfUsers.observe(this, { it->
+            for (i in it){
+                print("${i}")
+            }
+
+        })
 
         vm.listToShowUsers.observe(this, {
-         adapter= AccountAdapter(it)
-            recyclerView.adapter = adapter
+            adapter= AccountAdapter(it)
+            binding.recylerView.adapter = adapter
         })
 
     }
--- app/src/main/java/com/example/android/fetchproject/MainActivityViewModel.kt
+++ app/src/main/java/com/example/android/fetchproject/MainActivityViewModel.kt
@@ -1,6 +1,7 @@
 package com.example.android.fetchproject
 
 import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.Transformations
 import androidx.lifecycle.ViewModel
 import com.example.android.fetchproject.domain.DomainAccount
@@ -13,10 +14,43 @@ import kotlin.collections.HashMap
 
 class MainActivityViewModel : ViewModel() {
 
+    var showListIDOne:Boolean
+    var showListIDTwo: Boolean
+    var showListIDThree :Boolean
+    var showListIDFour: Boolean
+
+
     private val repository = Repository()
 
-    val listToShowUsers: LiveData<List<DomainAccount>> = Transformations.map(repository.mapOfAccountData) { partionedMap ->
-        sort(partionedMap)
+     val completeListOfUsers: LiveData<List<DomainAccount>> = Transformations.map(repository.mapOfAccountData) { partionedMap ->
+            sort(partionedMap)
+        }
+
+    private val _listToShowUsers = MutableLiveData<List<DomainAccount>>()
+    val listToShowUsers: LiveData<List<DomainAccount>> get() = _listToShowUsers
+
+
+    fun filter() {
+        var keysToShow = mutableListOf<Int>()
+        if (showListIDOne) keysToShow.add(1)
+        if (showListIDTwo) keysToShow.add(2)
+        if (showListIDThree) keysToShow.add(3)
+        if (showListIDFour) keysToShow.add(4)
+
+        println(" key  ${keysToShow}")
+        updateDisplay(keysToShow)
+    }
+
+
+    private fun updateDisplay(keysToShow: MutableList<Int>) {
+        val newList = ArrayList<DomainAccount>()
+        completeListOfUsers.value?.forEach {
+            if (keysToShow.contains(it.listId))
+                newList.add(it)
+        }
+
+        println(" new list ${newList}  ${completeListOfUsers.value.toString()} ")
+        _listToShowUsers.value = newList
     }
 
 
@@ -27,11 +61,12 @@ class MainActivityViewModel : ViewModel() {
             //checks to see if the list instance is null or not
             listOfAccountsInMap.get(key)?.let { dataTransferObjectList ->
                 //uses extension function (i created) to transform data to from FetchAccountDTO to FetchDomainObject
-                val domainObjectListOfAccounts = dataTransferObjectList.transformDataObjectToDomainObject()
+                val domainObjectListOfAccounts =
+                    dataTransferObjectList.transformDataObjectToDomainObject()
                 //Sort the list
                 Collections.sort(domainObjectListOfAccounts)
-               //add to the dataStructure
-                domainObjectListOfAccounts.forEach{it -> sortedAccounts.add(it)}
+                //add to the dataStructure
+                domainObjectListOfAccounts.forEach { it -> sortedAccounts.add(it) }
             }
         }
         //returns a list of all the FetchDomainAccounts in natural order
@@ -39,8 +74,11 @@ class MainActivityViewModel : ViewModel() {
     }
 
 
-
     init {
+        showListIDOne = false
+        showListIDTwo = false
+        showListIDThree = false
+        showListIDFour = false
         repository.getData()
     }
 }
